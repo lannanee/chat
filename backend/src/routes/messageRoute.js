6acd87731c8e656b37442ecbc63bd67c2bd278a1
lannanee <lannanhh0911@gmail.com>
2026-05-26 10:11:08 +0700

    update 11

--- backend/src/routes/messageRoute.js
+++ backend/src/routes/messageRoute.js
@@ -8,13 +8,12 @@ import {
   checkFriendship,
   checkGroupMembership,
 } from "../middlewares/friendMiddleware.js";
-import { protectedRoute } from "../middlewares/authMiddleware.js";
-import upload from "../middlewares/uploadMiddleware.js";
+import { upload } from "../middlewares/uploadMiddleware.js";
 
 const router = express.Router();
 
 router.post("/direct", checkFriendship, sendDirectMessage);
 router.post("/group", checkGroupMembership, sendGroupMessage);
-router.post("/upload-voice", protectedRoute, upload.single("voice"), uploadVoiceMessage);
+router.post("/upload-voice", upload.single("voice"), uploadVoiceMessage);
 
 export default router;
